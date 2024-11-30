5947b0bee7d28a032cd4e4d0856f06316c637123
bagomed <bogdan.efimov.05@mail.ru>
2024-11-30 21:16:52 +0300

    11.30 First Version

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { TopUI } from "./Components/TopUI/TopUI.tsx";
+
+export function App() {
+  return (
+    <div className="layout">
+      This is the very first version of my monkeytype copy!
+      <TopUI />
+    </div>
+  )
+}
+
+
---
+++ frontend/src/Components/TopUI/TopUI.tsx
@@ -0,0 +1,6 @@
+export function TopUI() {
+    return (
+        <div>
+        </div>
+    )
+}
